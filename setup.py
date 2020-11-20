ceb55e414c0ef3c21c48a95d17e1c8e95b65a534
ilchec <chechuro@shh.mpg.de>
2020-11-20 10:48:29 +0100

    changed names in setup.py

--- setup.py
+++ setup.py
@@ -7,20 +7,20 @@ with open("metadata.json", encoding="utf-8") as fp:
 
 
 setup(
-    name='lexibank_chechuroborrowing',
+    name='lexibank_daniellingua',
     version="1.0",
     description=metadata['title'],
     license=metadata.get('license', ''),
     url=metadata.get('url', ''),
-    py_modules=['lexibank_chechuroborrowing'],
+    py_modules=['lexibank_daniellinguafrancas'],
     include_package_data=True,
     zip_safe=False,
     entry_points={
         'lexibank.dataset': [
-            'chechuroborrowing=lexibank_chechuroborrowing:Dataset',
+            'daniellinguafrancas=lexibank_daniellinguafrancas:Dataset',
         ],
         'cldfbench.commands': [
-            'chechuroborrowing=commands',
+            'daniellinguafrancas=commands',
         ]
     },
     install_requires=[
