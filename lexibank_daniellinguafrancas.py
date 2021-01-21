7ace0fd94807a3f383c283a380aff84fda5e6432
Christoph Rzymski <christoph@functor-argument.net>
2021-01-21 12:59:57 +0100

    Refactor code

--- lexibank_daniellinguafrancas.py
+++ lexibank_daniellinguafrancas.py
@@ -1,12 +1,11 @@
 from pathlib import Path
 
-from pylexibank.dataset import Dataset as BaseDataset
-from pylexibank import progressbar
 import attr
 from clldutils.misc import slug
 from pylexibank import Concept, Language, Lexeme, FormSpec
 from pylexibank.dataset import Dataset as BaseDataset
 from pylexibank.util import progressbar
+
 
 @attr.s
 class CustomConcept(Concept):
@@ -23,59 +22,57 @@ class CustomLanguage(Language):
     List_ID = attr.ib(default=None)
     SubGroup = attr.ib(default=None)
 
+
 @attr.s
 class CustomLexeme(Lexeme):
     Stem = attr.ib(default=None)
     EntryId = attr.ib(default=None)
 
+
 class Dataset(BaseDataset):
     dir = Path(__file__).parent
     id = "daniellinguafrancas"
 
-    # add your personalized data types here
     concept_class = CustomConcept
     language_class = CustomLanguage
 
-    # define the way in which forms should be handled
     form_spec = FormSpec(
         brackets={"(": ")", "[": "]"},
         separators=";/,",
         missing_data=("?", "-"),
         strip_inside_brackets=True,
-        first_form_only=True
+        first_form_only=True,
     )
 
     def cmd_makecldf(self, args):
-        """
-        Convert the raw data to a CLDF dataset.
-        """
-        data = self.raw_dir.read_csv('words_data.tsv', dicts=True, delimiter='\t')
+        data = self.raw_dir.read_csv("words_data.tsv", dicts=True, delimiter="\t")
         args.writer.add_sources()
         languages = args.writer.add_languages(lookup_factory="ID")
-        sources = {l['ID']: l['Source'] for l in self.languages}
         concepts = {}
+
         for concept in self.concepts:
             idx = concept["NUMBER"].split("-")[-1] + "_" + slug(concept["ENGLISH"])
             args.writer.add_concept(
-                    ID=idx,
-                    Name=concept['ENGLISH'],
-                    Concepticon_ID=concept['CONCEPTICON_ID'],
-                    Concepticon_Gloss=concept['CONCEPTICON_GLOSS'],
-                    Russian_Gloss=concept['RUSSIAN'],
-                    Number=concept['NUMBER']
-                    )
-            concepts[concept['NUMBER']] = idx
+                ID=idx,
+                Name=concept["ENGLISH"],
+                Concepticon_ID=concept["CONCEPTICON_ID"],
+                Concepticon_Gloss=concept["CONCEPTICON_GLOSS"],
+                Russian_Gloss=concept["RUSSIAN"],
+                Number=concept["NUMBER"],
+            )
+            concepts[concept["NUMBER"]] = idx
 
         for row in progressbar(data, desc="cldfify"):
             lexemes = args.writer.add_forms_from_value(
-                    Language_ID=languages[row['Code']],
-                    Parameter_ID=concepts[row['Concept nr.']],
-                    Value=row['Standard Transcription'],
-                    Source=row['Source']
-                    )
+                Language_ID=languages[row["Code"]],
+                Parameter_ID=concepts[row["Concept nr."]],
+                Value=row["Standard Transcription"],
+                Source=row["Source"],
+            )
+
             args.writer.add_cognate(
-                    lexeme=lexemes[0],
-                    Cognateset_ID=row['Set'].replace(' ', ''),
-                    Cognate_Detection_Method='expert',
-                    Source=row['Source']
-                    )
+                lexeme=lexemes[0],
+                Cognateset_ID=row["Set"].replace(" ", ""),
+                Cognate_Detection_Method="expert",
+                Source=row["Source"],
+            )
--- setup.py
+++ setup.py
@@ -1,31 +1,24 @@
-from setuptools import setup
 import json
 
+from setuptools import setup
 
 with open("metadata.json", encoding="utf-8") as fp:
     metadata = json.load(fp)
 
 
 setup(
-    name='lexibank_daniellinguafrancas',
+    name="lexibank_daniellinguafrancas",
     version="1.0",
-    description=metadata['title'],
-    license=metadata.get('license', ''),
-    url=metadata.get('url', ''),
-    py_modules=['lexibank_daniellinguafrancas'],
+    description=metadata["title"],
+    license=metadata.get("license", ""),
+    url=metadata.get("url", ""),
+    py_modules=["lexibank_daniellinguafrancas"],
     include_package_data=True,
     zip_safe=False,
-    entry_points={
-        'lexibank.dataset': [
-            'daniellinguafrancas=lexibank_daniellinguafrancas:Dataset',
+    entry_points={"lexibank.dataset": [
+            "daniellinguafrancas=lexibank_daniellinguafrancas:Dataset",
         ],
     },
-    install_requires=[
-        'pylexibank>=1.1.1',
-    ],
-    extras_require={
-        'test': [
-            'pytest-cldf',
-        ],
-    },
+    install_requires=["pylexibank>=2.1"],
+    extras_require={"test": ["pytest-cldf"]},
 )
